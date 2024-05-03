beaca4d38684521df3ff3947817d1eaee6233875
cwongsatjachock <victorwongsatjachock@gmail.com>
2024-05-03 13:45:22 -0700

    exceptions check

--- reddit_crawler2.py
+++ reddit_crawler2.py
@@ -1,6 +1,8 @@
 import praw
 import json
 import config
+import prawcore
+import time
 
 reddit = praw.Reddit(
     username=config.username,
@@ -10,20 +12,28 @@ reddit = praw.Reddit(
     user_agent="CS172 Project Phase One"
 )
 
-subreddits = ["Home", "AskReddit", "NoStupidQuestions", "facepalm", "interestingasfuck", "Damnthatsinteresting", "AmItheAsshole", "mildlyinfuriating", "Piracy", "AITAH", "gaming", "worldnews", "pcmasterrace", "Unexpected", "news", "politics", "wallstreetbets", "todayilearned", "nottheonion", "explainlikeimfive", "OutOfTheLoop", "buildapc", "Steam", "badroommates", "personalfinance", "antiwork", "anime", "manga", "DnD", "technology", "unpopularopinion", "youtube", "legaladvice", "sysadmin", "relationship_advice", "discordapp", "pcgaming", "Games", "ChatGPT", "2007scape", "PiratedGames", "techsupport", "shitposting", "theydidthemath","cyberpunkgame", "OldSchoolCool", "coolguides", "AskMen", "SteamDeck", "college", "rareinsults", "science", "relationship"]
+subreddits = ["Home", "AskReddit", "NoStupidQuestions", "facepalm", "interestingasfuck", "Damnthatsinteresting", "AmItheAsshole", "mildlyinfuriating", "Piracy", "AITAH", "gaming", "worldnews", "pcmasterrace", "Unexpected", "news", "politics", "wallstreetbets", "todayilearned", "nottheonion", "explainlikeimfive", "OutOfTheLoop", "buildapc", "Steam", "badroommates", "personalfinance", "antiwork", "anime", "manga", "DnD", "technology", "unpopularopinion", "youtube", "legaladvice", "sysadmin", "relationship_advice", "discordapp", "pcgaming", "Games", "ChatGPT", "2007scape", "PiratedGames", "techsupport", "shitposting", "theydidthemath","cyberpunkgame", "OldSchoolCool", "coolguides", "AskMen", "SteamDeck", "college", "rareinsults", "science", "relationship", "csMajors", "ProgrammerHumor", "cscareerquestions", "Python", "cpp", "learnprogramming","leetcode", "computerscience"]
 
 outputFile = open("output.json", "w")
 
 outputFile.write("[\n")
 
+retry_delay = 60  # Initial delay is 5 seconds
+
 for index, subreddit in enumerate(subreddits):
-    top = list(reddit.subreddit(subreddit).top(limit=1000))
-    new = list(reddit.subreddit(subreddit).new(limit=1000))
-    rising = list(reddit.subreddit(subreddit).rising(limit=1000))
-    hot = list(reddit.subreddit(subreddit).hot(limit=1000))
+    for _ in range(5):  # Maximum of 5 attempts
+        try:
+            top = list(reddit.subreddit(subreddit).top(limit=1000))
+            new = list(reddit.subreddit(subreddit).new(limit=1000))
+            rising = list(reddit.subreddit(subreddit).rising(limit=1000))
+            hot = list(reddit.subreddit(subreddit).hot(limit=1000))
+            break  
+        except prawcore.exceptions.TooManyRequests as e:
+            time.sleep(retry_delay)
+            retry_delay *= 2 
+
     final = top + new + rising + hot
     final = set(final)
-    print(subreddit)
     for i, post in enumerate(final):
         post.comments.replace_more(limit=0)
         comments_data = [
@@ -31,7 +41,7 @@ for index, subreddit in enumerate(subreddits):
                 "author": comment.author.name if comment.author else 'deleted-user',
                 "body": comment.body
             }
-            for comment in post.comments[:100]
+            for comment in post.comments[:10]
         ]
         post_to_json = {
             "subreddit": subreddit,
